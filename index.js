1df9afae2d16c20be3e666e1f50bbde145e5fd79
Vincasmen <vincasmen13@gmail.com>
2022-06-05 17:08:58 +0200

    Finished the game

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
+"use strict";
+
 //Initial values
 let playerScore = document.querySelector(".player-score-number");
 let computerScore = document.querySelector(".computer-score-number");
 let winner = document.getElementById("winner-announcment");
+const showPlayerBtns = document.getElementById("players-choice");
+const againBtnDiv = document.getElementById("againBtn-div");
+const againBtn = document.getElementById("againBtn");
+const choice = document.querySelectorAll(".choice-player");
+const infoBtn = document.getElementById("info-icon");
+const modalWindow = document.getElementById("modal-div-id");
+const overlay = document.getElementById("overlay");
 
-playerScore.textContent = 0;
-computerScore.textContent = 0;
-winner.textContent = "";
+overlay.addEventListener("click", function () {
+  modalWindow.classList.toggle("hidden-modal");
+  overlay.classList.toggle("hidden-overlay");
+});
 
+//Event listener to info button
+infoBtn.addEventListener("click", function () {
+  modalWindow.classList.toggle("hidden-modal");
+  overlay.classList.toggle("hidden-overlay");
+});
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modalWindow.classList.contains("hidden-modal")) {
+    modalWindow.classList.toggle("hidden-modal");
+    overlay.classList.toggle("hidden-overlay");
+  }
+});
+
+const init = function () {
+  playerScore.textContent = 0;
+  computerScore.textContent = 0;
+  winner.textContent = "⬇️Start the game⬇️";
+  showPlayerBtns.classList.remove("hidden-player-btns");
+};
+init();
 //First I declared each of the choice(rock, paper, scissor).
 const rockPlayer = document.getElementById("rock");
 const paperPlayer = document.getElementById("paper");
@@ -21,6 +51,10 @@ const player = document.getElementById("player-choice");
 //Button to find out who won
 const resultBtn = document.getElementById("result");
 
+const playAgain = function () {
+  init();
+  againBtnDiv.classList.add("hidden");
+};
 //Div of players choices
 // const playersOptions = document.getElementById("players-choice");
 
@@ -30,99 +64,82 @@ const resultBtn = document.getElementById("result");
 //Array for computers choice
 const computerChoices = ["Rock 🪨", "Paper 📰", "Scissors ✂️"];
 //Click events for player options buttons
-const btns = [
-  rockPlayer.addEventListener("click", function () {
-    player.textContent = rockPlayer.textContent;
-  }),
+
+rockPlayer.addEventListener("click", function () {
+  player.textContent = rockPlayer.textContent;
+}),
   paperPlayer.addEventListener("click", function () {
     player.textContent = paperPlayer.textContent;
   }),
   scissorsPlayer.addEventListener("click", function () {
     player.textContent = scissorsPlayer.textContent;
   }),
-];
+  //Event listener for again button
+  againBtn.addEventListener("click", playAgain);
 
-for (let i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", zkouska());
+for (let i = 0; i < choice.length; i++) {
+  choice[i].addEventListener("click", function () {
+    //Randomize choice of the computer
+    const randomChoice = Math.floor(Math.random() * 3);
+    computer.textContent = computerChoices[randomChoice];
+
+    //Tie eventuality
+    if (computer.textContent === player.textContent) {
+      winner.textContent = "Tie ❌";
+      //Rule 1 🪨❌📰
+    } else if (player.textContent == "Rock 🪨") {
+      if (computer.textContent == "Paper 📰") {
+        //Computer win 🤖🎉
+        computerScore.textContent++;
+        winner.textContent = "Computer WIN 🤖🎉";
+        // console.log("Computer +1");
+      } else {
+        //Player win 😶🎉
+        playerScore.textContent++;
+        winner.textContent = "Player WIN 😶🎉";
+        // console.log("Player +1");
+      }
+      //Rule 2 ✂️❌🪨
+    } else if (player.textContent == "Scissors ✂️") {
+      if (computer.textContent == "Rock 🪨") {
+        //Computer win 🤖🎉
+        computerScore.textContent++;
+        winner.textContent = "Computer WIN 🤖🎉";
+        // console.log("Computer +1");
+      } else {
+        //Player win 😶🎉
+        playerScore.textContent++;
+        winner.textContent = "Player WIN 😶🎉";
+        // console.log("Player +1");
+      }
+      //Rule 3 📰❌✂️
+    } else if (player.textContent == "Paper 📰") {
+      if (computer.textContent == "Scissors ✂️") {
+        //Computer win 🤖🎉
+        computerScore.textContent++;
+        winner.textContent = "Computer WIN 🤖🎉";
+        // console.log("Computer +1");
+      } else {
+        //Player win 😶🎉
+        playerScore.textContent++;
+        winner.textContent = "Player WIN 😶🎉";
+        // console.log("Player +1");
+      }
+    }
+    endGame();
+  });
 }
-const zkouska = btns.addEventListener("click", function () {
-  //Randomize choice of the computer
-  const randomChoice = Math.floor(Math.random() * 3);
-  computer.textContent = computerChoices[randomChoice];
-
-  //Tie eventuality
-  if (computer.textContent === player.textContent) {
-    winner.textContent = "Tie ❌";
-    //Rule 1 🪨❌📰
-  } else if (player.textContent == "Rock 🪨") {
-    if (computer.textContent == "Paper 📰") {
-      //Computer win 🤖🎉
-      computerScore.textContent++;
-      winner.textContent = "Computer WIN 🤖🎉";
-      // console.log("Computer +1");
-    } else {
-      //Player win 😶🎉
-      playerScore.textContent++;
-      winner.textContent = "Player WIN 😶🎉";
-      // console.log("Player +1");
-    }
-    //Rule 2 ✂️❌🪨
-  } else if (player.textContent == "Scissors ✂️") {
-    if (computer.textContent == "Rock 🪨") {
-      //Computer win 🤖🎉
-      computerScore.textContent++;
-      winner.textContent = "Computer WIN 🤖🎉";
-      // console.log("Computer +1");
-    } else {
-      //Player win 😶🎉
-      playerScore.textContent++;
-      winner.textContent = "Player WIN 😶🎉";
-      // console.log("Player +1");
-    }
-    //Rule 3 📰❌✂️
-  } else if (player.textContent == "Paper 📰") {
-    if (computer.textContent == "Scissors ✂️") {
-      //Computer win 🤖🎉
-      computerScore.textContent++;
-      winner.textContent = "Computer WIN 🤖🎉";
-      // console.log("Computer +1");
-    } else {
-      //Player win 😶🎉
-      playerScore.textContent++;
-      winner.textContent = "Player WIN 😶🎉";
-      // console.log("Player +1");
-    }
-  }
-  computerScore.textContent >= 5
-    ? (winner.textContent = "Computer won the game")
-    : playerScore.textContent >= 5
-    ? (winner.textContent = "Player won the game")
-    : (winner.textContent = resultBtn());
-});
 
 //End Game rules
-
 const endGame = function () {
-  computerScore.textContent > 5
-    ? (winner.textContent = "Computer won the game")
-    : playerScore.textContent > 5
-    ? (winner.textContent = "Player won the game")
-    : (winner.textContent = resultBtn);
+  computerScore.textContent >= 5
+    ? (winner.textContent = "Computer won the game 👎👎👎")
+    : playerScore.textContent >= 5
+    ? (winner.textContent = "Player won the game 🎉🎉🎉")
+    : (winner.textContent = choice());
+  showPlayerBtns.classList.add("hidden-player-btns");
+  againBtnDiv.classList.remove("hidden");
 };
-const endGame2 = function () {
-  computerScore.textContent > 9
-    ? (winner.textContent = "Computer won the game")
-    : playerScore.textContent > 9
-    ? (winner.textContent = "Player won the game")
-    : (winner.textContent = "");
-};
-
-// const compare = function (computerChoice, playerChoice) {
-//   if (computerChoice === playerChoice) {
-//     return console.log("Tie");
-//   }
-// };
-// const output = compare(computer[randomChoice], player.textContent);
 
 //Longer way of creating an event handler for each of the button
 
